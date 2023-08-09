f1707d856bc2d5dfbca6f9d7956aec49966ca7c1
LadyDammy <oreoluwadammie@gmail.com>
2023-08-09 15:29:50 -0700

    Change of endpoint

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,6 +7,10 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
     return jsonify({"status": "OK"})
 
 
